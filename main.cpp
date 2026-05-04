7ea95101d78aae6aebecd24ffe47a5ef953d8d85
Spad0n <92205208+Spad0n@users.noreply.github.com>
2026-05-04 22:13:36 +0200

    add support for reading files

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include "ctl/system.hpp"
 #include "ctl/allocator.hpp"
+#include "ctl/file.hpp"
 #include "lexer.hpp"
 #include "parser.hpp"
 #include "assembler.hpp"
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 #define  VMIPS32_IMPLEMENTATION
 #include "vmips32.h"
@@ -12,9 +12,12 @@
 extern "C" {
     
     void vmips32_syscall(uint32_t v0, uint32_t a0, uint32_t a1, uint32_t a2, uint32_t a3) {
+        InlineAllocator<2048> alloc;
+        StringBuilder sb{alloc};
         switch (v0) {
         case 1: // Print Integer
-            printf("%d", (int32_t)a0);
+            sb.put((int32_t)a0);
+            Console::print(*sb.result());
             break;
 
         case 4: { // Print String
@@ -29,140 +32,71 @@ extern "C" {
                 char c = (char)bytes[3 - addr % 4];
 
                 if (c == '\0') break;
-                putchar(c);
+                Console::print(StringView(&c));
                 addr += 1;
             }
             break;
         }
 
         case 10: // Exit standard
-            printf("\n--- Program exited (syscall 10) ---\n");
-            exit(0);
+            Console::print("\n--- Program exited (syscall 10) ---\n");
+            _exit(0);
             break;
 
         case 17: // Exit with value
-            printf("\n--- Program exited with code %d (syscall 17) ---\n", a0);
-            exit(a0);
+            sb.format("\n--- Program exited with code %d (syscall 17) ---\n", a0);
+            Console::print(*sb.result());
+            _exit(a0);
             break;
 
         case 11: // Print Character
-            putchar((char)(a0 & 0xFF));
+            sb.put((char)(a0 & 0xFF));
+            Console::print(*sb.result());
             break;
 
         default:
             // Debug : check which syscall is called
-            printf("\n[Syscall] Unknown syscall code: %u (a0: %u)\n", v0, a0);
+            sb.format("\n[Syscall] Unknown syscall code: %u (a0: %u)\n", v0, a0);
+            Console::print(*sb.result());
             break;
         }
     }
 
     void vmips32_trap(const char *message) {
-        fprintf(stderr, "%s", message);
-        exit(1);
+        Console::print(StringView(message));
+        _exit(1);
     }
 }
 
 using namespace ctl;
 
-Sint32 main() {
+Sint32 main(Sint32 argc, char **argv) {
     SystemAllocator sys;
     TemporaryAllocator alloc{sys};
 
-    StringView src = 
-        ".data\n"
-        "    msg_start:  .asciiz \"Calcul de 5! : \"\n"
-        "    msg_bit:    .asciiz \"Test Bitwise: \"\n"
-        "    msg_res:    .asciiz \"\\nResultat final: \"\n"
-        "    storage:    .word 0\n"
-        "\n"
-        ".text\n"
-        ".globl main\n"
-        "\n"
-        "main:\n"
-        "    li $sp, 0x00401000\n"
-        "\n"
-        "    la $a0, msg_start\n"
-        "    li $v0, 4\n"
-        "    syscall\n"
-        "    nop\n"
-        "\n"
-        "    li $t0, 0xAA\n"
-        "    li $t1, 0x55\n"
-        "    or $t2, $t0, $t1\n"
-        "    and $t3, $t2, $t0\n"
-        "    xor $t4, $t3, $t0\n"
-        "\n"
-        "    li $s0, 0\n"
-        "    bnez $t4, fail_bit\n"
-        "    nop\n"
-        "    li $s0, 1\n"
-        "\n"
-        "fail_bit:\n"
-        "    la $a0, msg_bit\n"
-        "    li $v0, 4\n"
-        "    syscall\n"
-        "    nop\n"
-        "\n"
-        "    move $a0, $s0\n"
-        "    li $v0, 1\n"
-        "    syscall\n"
-        "    nop\n"
-        "\n"
-        "    li $a0, 5\n"
-        "    jal factorial\n"
-        "    nop\n"
-        "\n"
-        "    la $t8, storage\n"
-        "    sw $v0, 0($t8)\n"
-        "\n"
-        "    la $a0, msg_res\n"
-        "    li $v0, 4\n"
-        "    syscall\n"
-        "    nop\n"
-        "\n"
-        "    lw $a0, 0($t8)\n"
-        "    li $v0, 1\n"
-        "    syscall\n"
-        "    nop\n"
-        "\n"
-        "    li $v0, 17\n"
-        "    li $a0, 69\n"
-        "    syscall\n"
-        "    nop\n"
-        "\n"
-        "factorial:\n"
-        "    addiu $sp, $sp, -8\n"
-        "    sw $ra, 4($sp)\n"
-        "    sw $a0, 0($sp)\n"
-        "\n"
-        "    li $t0, 1\n"
-        "    ble $a0, $t0, fact_base\n"
-        "    nop\n"
-        "\n"
-        "    addiu $a0, $a0, -1\n"
-        "    jal factorial\n"
-        "    nop\n"
-        "\n"
-        "    lw $a0, 0($sp)\n"
-        "    lw $ra, 4($sp)\n"
-        "    addiu $sp, $sp, 8\n"
-        "    mult $v0, $a0\n"
-        "    mflo $v0\n"
-        "    jr $ra\n"
-        "    nop\n"
-        "\n"
-        "fact_base:\n"
-        "    li $v0, 1\n"
-        "    addiu $sp, $sp, 8\n"
-        "    jr $ra\n"
-        "    nop\n";
+    if (argc < 2) {
+        Console::print("Usage: ./mips <file.mips>\n");
+        return 1;
+    }
+
+    auto file = File::open(argv[1], File::Access::RD);
+    if (!file) {
+        Console::print("ERROR: could not open the file\n");
+        return 1;
+    }
+    Array<Uint8> content = file->map(alloc);
+    StringView src = content.slice().cast<const char>();
 
     Lexer lex{src, alloc};
     auto tokens = lex.tokenize();
 
+    StringBuilder sb{alloc};
+
     if (!tokens || lex.has_error()) {
         StringView msg = lex.error_message();
-        fprintf(stderr, "ERROR: %.*s\n", msg.length(), msg.data());
+        Location loc = lex.error_location();
+        sb.format("%s:%d:%d: %.*s\n", argv[1], loc.line, loc.column, msg.length(), msg.data());
+        Console::print(*sb.result());
         return 1;
     }
 
@@ -170,9 +104,11 @@ Sint32 main() {
     parser.parse();
     if (parser.has_errors()) {
         for (auto& e : parser.errors()) {
+            sb.reset();
             Location loc = e.loc;
             StringView msg = e.message;
-            fprintf(stderr, "error:%d:%d: %.*s\n", loc.line, loc.column, msg.length(), msg.data());
+            sb.format("%s:%d:%d: %.*s\n", argv[1], loc.line, loc.column, msg.length(), msg.data());
+            Console::print(*sb.result());
         }
         return 1;
     }
@@ -181,9 +117,11 @@ Sint32 main() {
     asm_.assemble(parser.stmts(), parser.operands());
     if (asm_.has_errors()) {
         for (auto& e : asm_.errors()) {
+            sb.reset();
             Location loc = e.loc;
             StringView msg = e.message;
-            fprintf(stderr, "error:%d:%d: %.*s\n", loc.line, loc.column, msg.length(), msg.data());
+            sb.format("%s:%d:%d: %.*s\n", argv[1], loc.line, loc.column, msg.length(), msg.data());
+            Console::print(*sb.result());
         }
         return 1;
     }
